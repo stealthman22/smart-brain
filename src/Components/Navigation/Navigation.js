fbc7185e3bdf9892e5e53074e88c2de8ce40e331
stealthman22 <tdotonline@gmail.com>
2019-10-01 22:24:37 +0000

    Added jsx attributes that connect it to the routes

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-const Navigation = () => {
-    return (
-        <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-        </nav>
-    )
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+
+    if (isSignedIn) {
+        return (
+            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+            </nav>
+        )
+    } else {
+        return (
+            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+                <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Regster</p>
+            </nav>
+        )
+    }
+
+
 }
 
 export default Navigation;
+
+/*
+line 6 : signin is a parameter
+
+The conditional in this component describes the state of the app at home
+*/
