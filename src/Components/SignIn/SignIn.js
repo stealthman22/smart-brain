09b65f079359bcdda7c6a52b08ad585d0110a436
stealthman22 <tdotonline@gmail.com>
2019-10-18 05:43:12 +0000

    Built routes and states that connect back end signing endpoint to it

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,39 +1,94 @@
 import React from 'react';
 
 
-const SignIn = ({ onRouteChange }) => {
-    return (
-        <div>
-            <article className="br3 ba  b--black-10 mv6 w-100 w-50-m w-25-l mw5  shadow-5 center">
-                <main className="pa4 black-80">
-                    <div className="measure ">
-                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                            <div className="mt3">
-                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
-                            </div>
-                            <div className="mv3">
-                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
-                            </div>
+class SignIn extends React.Component {
+    //pass props to both constructor and super for it to work
+    constructor(props) {
+        super(props);
+        this.state = {
+            signInEmail: '',
+            signInPassword: ''
+        }
+    }
+    // To get the input from both fields.
+    onEmailChange = (event) => {
+        this.setState({ signInEmail: event.target.value })
+    }
 
-                        </fieldset>
-                        <div onClick={() => onRouteChange('home')}
-                            className="">
-                            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                                type="submit"
-                                value="Sign in" />
-                        </div>
-                        <div className="lh-copy mt3">
-                            <p onClick={() => onRouteChange('register')} className="f6 link dim black db  pointer">Register</p>
-                        </div>
-                    </div>
+    onPasswordChange = (event) => {
+        this.setState({ signInPassword: event.target.value })
+    }
 
-                </main>
-            </article>
-        </div >
-    )
+    onSubmitSignIn = () => {
+        // function to send the sign in request to the server
+        // configure for post req, using second argument
+        fetch('http://localhost:3000/signin', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: this.state.signInEmail,
+                password: this.state.signInPassword
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data === 'success') {
+                    this.props.onRouteChange('home');
+                }
+            })
+    }
+
+    render() {
+        const { onRouteChange } = this.props;
+        return (
+            <div>
+                <article className="br3 ba  b--black-10 mv6 w-100 w-50-m    w-25-l mw5  shadow-5 center">
+                    <main className="pa4 black-80">
+                        <div className="measure ">
+                            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                                <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                                <div className="mt3">
+                                    <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                                    <input
+                                        className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                        type="email"
+                                        name="email-address" id="email-address"
+                                        // pass the  event to grab email change 
+                                        onChange={this.onEmailChange}
+                                    />
+                                </div>
+                                <div className="mv3">
+                                    <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                                    <input
+                                        className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        // pass the  event to grab email change 
+                                        onChange={this.onPasswordChange}
+                                    />
+                                </div>
+                            </fieldset>
+
+                            <div onClick={() => onRouteChange('home')}
+                                className="">
+                                <input
+                                    // Event for signin
+                                    onClick={this.onSubmitSignIn}
+                                    className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                                    type="submit"
+                                    value="Sign in" />
+                            </div>
+                            <div className="lh-copy mt3">
+                                <p onClick={() => onRouteChange('register')} className="f6 link dim black db  pointer">Register</p>
+                            </div>
+                        </div>
+
+                    </main>
+                </article>
+            </div>
+        )
+    }
 }
 
 export default SignIn;
