7bbcb52b555f7abec3f3a68cb0664bfbc0a712e8
stealthman22 <tdotonline@gmail.com>
2019-10-22 23:43:20 +0000

    Added few comments

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// Automstically updates name and score values of users
+// Automatically updates name and score values of users
 const Rank = ({ name, entries }) => {
     return (
 
