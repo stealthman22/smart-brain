4651f177888655e1c1a51ef97fc7d2e52b35b642
stealthman22 <tdotonline@gmail.com>
2019-11-03 23:53:22 +0000

    Added few edits

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -77,7 +77,7 @@ class SignIn extends React.Component {
                                 <input
                                     // Event for signin
                                     onClick={this.onSubmitSignIn}
-                                    className="b ph3 pv2                              input-reset ba b--black bg-transparent grow pointer f6 dib"
+                                    className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                                     type="submit"
                                     value="Sign in" />
                             </div>
