98b8b4b1bf5d27b19a5be2f42ec7d1041959c001
stealthman22 <tdotonline@gmail.com>
2019-11-06 10:01:42 +0000

    Changing the  fetch adress to point to heroku app

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
     // Life cycle hook to connect with server connection
     // lifecycle hooks don't need arrow functions.
     // componentDidMount() {
-    //     fetch('http://localhost:3000/')
+    //     fetch('https://stormy-tundra-86347.herokuapp.com/:3000/')
     //         .then(response => response.json())
     //         .then(console.log);
     // }
@@ -128,7 +128,7 @@ class App extends Component {
         // return value of calc method is needed by display method to render bounding box
 
         // for clarifai
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://stormy-tundra-86347.herokuapp.com/imageurl', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
@@ -138,7 +138,7 @@ class App extends Component {
             .then(response => response.json())
             .then(response => {
                 if (response) {
-                    fetch('http://localhost:3000/image', {
+                    fetch('https://stormy-tundra-86347.herokuapp.com/:3000/image', {
                         method: 'put',
                         headers: { 'Content-Type': 'application/json' },
                         body: JSON.stringify({
